e174c12c7b18dd1c88a4f34df128c6774b37750a
mohamedriadh.khalfallah <mohamedriadh.khalfallah@sofrecom.com>
2022-03-25 21:32:16 +0100

    integration twig

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/templates');
+$twig = new \Twig\Environment($loader, [
+//    'cache' => __DIR__.'/compilation_cache',
+]);
+
+echo $twig->render('index.html', ['name' => 'Fabien']);
