1967af6df49c9b98383fd68d1d1ae7e7722bab20
mohamedriadh.khalfallah <mohamedriadh.khalfallah@sofrecom.com>
2022-03-29 02:18:22 +0200

    add docker and add routing in project

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?php
 require 'vendor/autoload.php';
 
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/templates');
-$twig = new \Twig\Environment($loader, [
-//    'cache' => __DIR__.'/compilation_cache',
-]);
 
-echo $twig->render('index.html.twig', ['name' => 'Fabien']);
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+    case '' :
+        $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
+        $twig = new \Twig\Environment($loader, [
+//    'cache' => __DIR__.'/compilation_cache',
+        ]);
+
+        echo $twig->render('index.html.twig', ['name' => 'Fabien']);
+        break;
+    case '/refclics' :
+        require __DIR__ . '/refclics.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
